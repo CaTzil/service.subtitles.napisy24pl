98f11d1749e14b7243b4705e5f86deac9a35483d
CaTz <catzil@gmail.com>
2024-07-14 16:09:14 +0300

    update user agent version

--- resources/lib/NapisyUtils.py
+++ resources/lib/NapisyUtils.py
@@ -371,7 +371,7 @@ class URLHandler:
             ("Cache-Control", "no-cache"),
             (
                 "User-Agent",
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
             ),
         ]
 
